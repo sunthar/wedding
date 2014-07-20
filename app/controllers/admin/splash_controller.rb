f50e3d569fb7a12ab68679bcd660e248591da381
Zach <zach@piazza.com>
2014-07-20 15:28:59 -0700

    fixing export of data

--- app/controllers/admin/splash_controller.rb
+++ app/controllers/admin/splash_controller.rb
@@ -37,11 +37,14 @@ class Admin::SplashController < AdminController
   end
 
   def export
+    fields = ["first_name", "last_name", "full_name", "salutation", "email", "access_code", "phone", "street_address", "city", "state", "zip", "other_people", "invited_to_ceremony", "invited_to_reception", "invited_by", "invitee_batch_a", "invitee_batch_b", "rsvp_ceremony", "rsvp_reception", "rsvp_sent", "reminder_sent", "wedding_info_sent", "table_number"]
     filename = "data-#{Time.now.strftime("%m-%d-%Y_%H-%M-%S")}"
-    csv = ["first_name", "last_name", "full_name", "salutation", "email", "access_code", "phone", "street_address", "city", "state", "zip", "other_people", "invited_to_ceremony", "invited_to_reception", "invited_by", "invitee_batch_a", "invitee_batch_b", "rsvp_ceremony", "rsvp_reception", "rsvp_sent", "reminder_sent", "wedding_info_sent", "table_number"].join(",") + "\n"
+    csv = fields.join(",") + "\n"
     id_to_other_names = {}
     User.all.each do |u|
-      csv += ([u.first_name, u.last_name, u.full_name, u.salutation, u.email, u.access_code, u.phone, u.street_address, u.city, u.stat, u.zip, u.other_people.join(", "), u.invited_to_ceremony, u.invited_to_reception, u.invited_by, u.invitee_batch_a, u.invitee_batch_b, u.rsvp_ceremony, u.rsvp_reception, u.rsvp_sent, r.reminder_sent, u.wedding_info_sent, u.table_number].join(",") + "\n")
+      field_array = []
+      fields.each{|f| field_array.push(u[f])}
+      csv += (field_array.join(",") + "\n")
     end
     send_data(csv, :type => 'text/csv', :disposition => "attachment; filename=#{filename}")
   end
